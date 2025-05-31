373c831f0943a7492ac5a6b1dcf5fd37b125ae3f
xasipos <alexandrosastros@gmail.com>
2025-05-31 15:59:36 +0300

    initializeGameBoard

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,13 +1,20 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class TicTacToe {
     public static void main(String[] args) {
 
         System.out.println("Καλοσήρθατε στον κόσμο της Τρίλιζας!");
+        System.out.println("πόσο επί ποσο θες να παιξεις Τρίλιζα?");
+        Scanner keyboard = new Scanner(System.in);
+        int row = keyboard.nextInt();
+        int col = keyboard.nextInt();
+        //System.out.println(row + " και " + col);
 
-        String[][] gameBoard = new String[5][5];
 
-        initializeGameBoard(gameBoard);
+        String[][] mygameBoard = new String[row][col];
+
+        initializeGameBoard(mygameBoard);
 
 
     }// end of main
@@ -18,26 +25,50 @@ public class TicTacToe {
 
     public static void initializeGameBoard(String[][] gameBoard){
 
-        for (int row=0; row<=4; row++){
-            if (row==0 || row==2 || row ==4) {
-                for (int col = 0; col <= 4; col++) {
-                    gameBoard[row][col] = "   |   |   ";
-                    System.out.println();
+//        gameBoard[0][0] = "00";
+//        gameBoard[0][1] = "01";
+//        gameBoard[0][2] = "02";
+//        gameBoard[1][0] = "10";
+//        gameBoard[1][1] = "11";
+//        gameBoard[1][2] = "12";
+//        gameBoard[2][0] = "20";
+//        gameBoard[2][1] = "21";
+//        gameBoard[2][2] = "22";
+
+        System.out.println("το μεγεθος του gameboard ειναι " + gameBoard.length + "x" + gameBoard.length);
+        System.out.print("τα ονοματα στα τετραγωνα ειναι: ");
+        for (int row = 0; row < gameBoard.length; row++){
+            for (int col = 0; col < gameBoard.length; col++){
+                gameBoard[row][col] = row + "" + col;
+
+                System.out.print(gameBoard[row][col] + ", ");
+                //System.out.println(row + " kai " + col);
+
+            }
+        }
+
+        System.out.println();
+
+        for (int row = 0; row < gameBoard.length; row++){
+            for (int col = 0; col < gameBoard.length; col++){
+                System.out.print(gameBoard[row][col]);
+                if (col != gameBoard.length-1) {
+                    System.out.print("|");
                 }
             }
+            System.out.println();
 
-            else{
-                for (int col=0; col<=4; col++){
-                gameBoard[row][col] = "---+---+---";
-                System.out.println();
-            }
-            }
-        }
-        for (int row = 0; row <= 4; row++){
-            for (int col = 0; col <= 4; col++){
-                System.out.println(gameBoard[row][col]);
+
+            if (row != gameBoard.length-1) {
+                String k = "--+";
+
+                String str = k.repeat(gameBoard.length);
+                //System.out.println("Str " + str);
+                System.out.println(str.substring(0, str.length()-1));
+
             }
         }
+
 
 
 
