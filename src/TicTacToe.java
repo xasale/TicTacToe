7181a8d699c03b202e4df69d7a5020ee76c203cb
xasipos <alexandrosastros@gmail.com>
2025-06-22 19:02:22 +0300

    final

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -18,7 +18,7 @@ public class TicTacToe {
             printInitializedBoard(mygameBoard);
             boolean x = true;
             getUserInput(x,mygameBoard);
-            //System.out.println("εφτασε εδω");
+
         }
         else {
             System.out.println("θα πρέπει οι αριθμοί να είναι ίδιοι");
@@ -70,31 +70,59 @@ public class TicTacToe {
     }// end printInitializedBoard
 
     public static void getUserInput(boolean xTurn,String[][] gameBoard){
-        for (int i=1; i<= (gameBoard.length^2); i++) {
+        int totalMoves = gameBoard.length * gameBoard.length;
+        for (int i=1; i<=(totalMoves); i++) {
+            //System.out.println("Μπηκε: " + i + " φορά");
             if (xTurn) {
                 System.out.println("Παίζει ο παίχτης με το Χ");
                 System.out.print("Σε ποιο τετράγωνο θες να παίξεις; ");
                 System.out.println("Βάλε γραμμή και στήλη");
                 Scanner keyboard = new Scanner(System.in);
-                int myrow = keyboard.nextInt();
-                if (myrow> gameBoard.length){
-                    System.out.println("Έδωσες λάθος γραμμή. Ξανα προσπάθησε.");
-                    getUserInput(xTurn, gameBoard);
-                }
-                int mycol = keyboard.nextInt();
-                if (mycol> gameBoard.length){
-                    System.out.println("Έδωσες λάθος στήλη. Ξανα προσπάθησε.");
-                    getUserInput(xTurn, gameBoard);
-                }
-                if (cellAlreadyOccupied(myrow, mycol, gameBoard)) {
-                    if (!isBoardFull(gameBoard)) {
-                        System.out.println("Δωσε ξανα. Εκει που θες να βαλεις εχει ηδη παιχτει.");
-                        getUserInput(xTurn, gameBoard);
+                int myrow;
+
+//                if (myrow>= gameBoard.length){
+//                    System.out.println("Έδωσες λάθος γραμμή. Ξανα προσπάθησε.");
+//                    getUserInput(xTurn, gameBoard);
+//                }
+
+                do {
+                    System.out.print("Γράψε γραμμή: ");
+                    myrow = keyboard.nextInt();
+                    if (myrow < 0 || myrow >= gameBoard.length) {
+                        System.out.println("Έδωσες λάθος γραμμή. Ξαναπροσπάθησε.");
                     }
+                } while (myrow < 0 || myrow >= gameBoard.length);
+
+                int mycol;
+
+//                if (mycol>= gameBoard.length){
+//                    System.out.println("Έδωσες λάθος στήλη. Ξανα προσπάθησε.");
+//                    getUserInput(xTurn, gameBoard);
+//                }
+
+                do {
+                    System.out.print("Γράψε στήλη: ");
+                    mycol = keyboard.nextInt();
+                    if (mycol < 0 || mycol >= gameBoard.length) {
+                        System.out.println("Έδωσες λάθος στήλη. Ξαναπροσπάθησε.");
+                    }
+                } while (mycol < 0 || mycol >= gameBoard.length);
+
+                if (cellAlreadyOccupied(myrow, mycol, gameBoard)) {
+
+                        System.out.println("Δωσε ξανα. Εκει που θες να βαλεις εχει ηδη παιχτει.");
+                        printCurrentBoard(gameBoard);
+                        getUserInput(xTurn, gameBoard);
+
                 } else {
-                    gameBoard[myrow][mycol] = "X ";
-                    getWinner(myrow,mycol,gameBoard);
+                    gameBoard[myrow][mycol] = " X ";
                     printCurrentBoard(gameBoard);
+                    if (isBoardFull(gameBoard)){
+                        break;
+                    }
+                    if (Objects.equals(getWinner(myrow, mycol, gameBoard), "victory!")){
+                        break;
+                    }
                 }
 
             } else {
@@ -102,18 +130,39 @@ public class TicTacToe {
                 System.out.print("Σε ποιο τετράγωνο θες να παίξεις; ");
                 System.out.println("Βάλε γραμμή και στήλη");
                 Scanner keyboard = new Scanner(System.in);
-                int myrow = keyboard.nextInt();
-                int mycol = keyboard.nextInt();
+                int myrow;
+                do {
+                    System.out.print("Γράψε γραμμή: ");
+                    myrow = keyboard.nextInt();
+                    if (myrow < 0 || myrow >= gameBoard.length) {
+                        System.out.println("Έδωσες λάθος γραμμή. Ξαναπροσπάθησε.");
+                    }
+                } while (myrow < 0 || myrow >= gameBoard.length);
+
+                int mycol;
+                do {
+                    System.out.print("Γράψε στήλη: ");
+                    mycol = keyboard.nextInt();
+                    if (mycol < 0 || mycol >= gameBoard.length) {
+                        System.out.println("Έδωσες λάθος στήλη. Ξαναπροσπάθησε.");
+                    }
+                } while (mycol < 0 || mycol >= gameBoard.length);
 
                 if (cellAlreadyOccupied(myrow, mycol, gameBoard)) {
-                    if (!isBoardFull(gameBoard)) {
+
                         System.out.println("Δωσε ξανα. Εκει που θες να βαλεις εχει ηδη παιχτει.");
+                        printCurrentBoard(gameBoard);
                         getUserInput(xTurn, gameBoard);
-                    }
+
                 } else {
-                    gameBoard[myrow][mycol] = "Y ";
-                    getWinner(myrow,mycol,gameBoard);
+                    gameBoard[myrow][mycol] = " Y ";
                     printCurrentBoard(gameBoard);
+                    if (isBoardFull(gameBoard)){
+                        break;
+                    }
+                    if (Objects.equals(getWinner(myrow, mycol, gameBoard), "victory!")){
+                        break;
+                    }
                 }
             }
             xTurn = !xTurn;
@@ -125,12 +174,12 @@ public class TicTacToe {
 
     public static void printCurrentBoard(String[][] gameBoard){
         printInitializedBoard(gameBoard);
-        System.out.println("μπηκε printCurrentBoard");
+        //System.out.println("μπηκε printCurrentBoard");
     }
 
     public static boolean cellAlreadyOccupied(int row, int col, String[][] gameBoard){
 
-        if (Objects.equals(gameBoard[row][col], "X") || Objects.equals(gameBoard[row][col], "Y")){
+        if (Objects.equals(gameBoard[row][col], " X ") || Objects.equals(gameBoard[row][col], " Y ")){
             return true;
         }
         else {
@@ -144,7 +193,7 @@ public class TicTacToe {
         String result = "";
 
         //checking the diagonials
-        if (selectedRow == selectedCol) {
+        if ((selectedRow == selectedCol) || (selectedRow + selectedCol == gameBoard.length-1)) {
 
             boolean majorDiag = true;
             boolean minorDiag = true;
@@ -154,7 +203,7 @@ public class TicTacToe {
             String tempMin = gameBoard[0][length - 1];
 
             // Check major diagonal, and update the boolean if our assumption is wrong.
-            for (int i = 1; i <= length; i++) {
+            for (int i = 1; i < length; i++) {
                 if (!Objects.equals(gameBoard[i][i], tempMaj)) {
                     majorDiag = false;
                     break;
@@ -162,7 +211,7 @@ public class TicTacToe {
             }
 
             // Check minor diagonal, and update the boolean if our assumption is wrong.
-            for (int i = 1, j = length - 2; i <= length; i++, j--) {
+            for (int i = 1, j = length - 2; i < length; i++, j--) {
                 if (!Objects.equals(gameBoard[i][j], tempMin)) {
                     minorDiag = false;
                     break;
@@ -181,25 +230,34 @@ public class TicTacToe {
 
             //checking the row & col
             String checkingPoint = gameBoard[selectedRow][selectedCol];
-            boolean same = true;
+            boolean sameRow = true;
+            boolean sameCol = true;
 
             //checking the Row
-            for (int i = 0; i <= length; i++) {
+            for (int i = 0; i < length; i++) {
                 if (!Objects.equals(gameBoard[selectedRow][i], checkingPoint)) {
-                    same = false;
+                    sameRow = false;
                     break;
                 }
             }
 
             //checking the Col
-            for (int i = 0; i <= length; i++) {
+            for (int i = 0; i < length; i++) {
                 if (!Objects.equals(gameBoard[i][selectedCol], checkingPoint)) {
-                    same = false;
+                    sameCol = false;
                     break;
                 }
             }
+//            System.out.println(sameRow);
+//            System.out.println(sameCol);
+            if (sameRow || sameCol) {
+                result = "victory!";
+            } else {
+                result = "παπαλα";
+            }
         }
 
+        System.out.println(result);
         return result;
 
 
@@ -208,15 +266,18 @@ public class TicTacToe {
     public static boolean isBoardFull(String[][] gameBoard){
 
         int count = 0;
+        int totalMoves = gameBoard.length * gameBoard.length;
 
         for (int row = 0; row < gameBoard.length; row++) {
             for (int col = 0; col < gameBoard.length; col++) {
-                if (Objects.equals(gameBoard[row][col], "X") || Objects.equals(gameBoard[row][col], "Y")) {
+                if (Objects.equals(gameBoard[row][col], " X ") || Objects.equals(gameBoard[row][col], " Y ")) {
                     count++;
                 }
             }
         }
-        if (count == (gameBoard.length^2)) {
+        System.out.println("count: " + count);
+        if (count == (totalMoves)) {
+            System.out.println("Γέμισε το τερεν");
             return true;
         }
         else{
